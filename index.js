7d46e873888c711273c947dc9912b76d8858ead0
sesdeneme643-dot <sesdeneme643@gmail.com>
2026-05-03 01:18:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,48 @@ const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
 app.get("/", (req, res) => {
-  res.send("Radyo Chat çalışıyor");
+  res.send(`
+<h2>Radyo Chat</h2>
+
+<input id="isim" placeholder="Adın">
+<button onclick="gir()">Giriş</button>
+
+<ul id="users"></ul>
+
+<div id="chat"></div>
+
+<input id="mesaj" placeholder="Mesaj">
+<button onclick="gonder()">Gönder</button>
+
+<script src="/socket.io/socket.io.js"></script>
+
+<script>
+const socket = io();
+let isim = "";
+
+function gir(){
+  isim = document.getElementById("isim").value;
+  socket.emit("kullanici", isim);
+}
+
+function gonder(){
+  let mesaj = document.getElementById("mesaj").value;
+  socket.emit("mesaj", isim + ": " + mesaj);
+}
+
+socket.on("mesaj", (data)=>{
+  document.getElementById("chat").innerHTML += "<p>"+data+"</p>";
+});
+
+socket.on("kullanicilar", (users)=>{
+  let html = "";
+  users.forEach(u=>{
+    html += "<li>"+u.isim+"</li>";
+  });
+  document.getElementById("users").innerHTML = html;
+});
+</script>
+`);
 });
 
 let users = [];
